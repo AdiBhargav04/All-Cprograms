d7f32be40215909e2bea54987d6f7401fd339755
AdiBhargav04 <adityabhargavattivilli@gmail.com>
2023-08-20 11:20:50 +0530

    hi

--- Python/File Handling/FH_programs.py
+++ Python/File Handling/FH_programs.py
@@ -13,12 +13,14 @@ print(file.read())
 
 # WAP to count the number of lines in the text file:
 '''
-filename=input("Enter the file name: ")
-c=0
-with open(filename,"r") as file:
-    for i in file:
-        c+=1
-print(c)
+filename = input("Enter file name: ")
+
+numLines = 0
+with open(filename, "r") as file:
+    for line in file:
+        numLines = numLines+1
+        
+print(numLines) 
 '''
 
 # Wap to count no of blank spaces in a text file
@@ -35,6 +37,33 @@ print(space+1)
 '''
 #WAP to count the occurance of a word in a text file
 '''
+filename = input("Enter file name: ")
+searchWord = input("Enter word: ")
+count=0
+
+with open(filename, "r") as file:
+    for line in file:
+        words = line.split()
+        for i in words:
+            if i==searchWord:
+                count=count+1
+print(count)
+'''
+# create a new file 
+# write two statements
+# close the file
+
+"""
+file = open("Ravi.txt","r")
+file.write("We are learning Python \n")
+file.write("Silicon Institute")
+file.close()
+"""
+'''
+file = open("Ravi.txt","w")
+myList = ["We are learning Python \n", "Silicon Institute"]
+file.writelines(myList)
+file.close()
 
 '''
 
